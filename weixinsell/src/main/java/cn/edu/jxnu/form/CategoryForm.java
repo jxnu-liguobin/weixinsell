52371fd7ef66c5e2fd02fde4c35b26be1434e056
jxnu-liguobin <568845948@qq.com>
2018-06-02 13:15:13 +0800

    修改注释

--- weixinsell/src/main/java/cn/edu/jxnu/form/CategoryForm.java
+++ weixinsell/src/main/java/cn/edu/jxnu/form/CategoryForm.java
@@ -3,17 +3,20 @@ package cn.edu.jxnu.form;
 import lombok.Data;
 
 /**
- * Created by 廖师兄
- * 2017-07-23 21:43
+ * 商品类目表单
+ * 
+ * @author 梦境迷离.
+ * @version V1.0
+ * @time 2018年6月2日
  */
 @Data
 public class CategoryForm {
 
-    private Integer categoryId;
+	private Integer categoryId;
 
-    /** 类目名字. */
-    private String categoryName;
+	/** 类目名字. */
+	private String categoryName;
 
-    /** 类目编号. */
-    private Integer categoryType;
+	/** 类目编号. */
+	private Integer categoryType;
 }
